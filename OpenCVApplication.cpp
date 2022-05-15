0d74544e64bba6ee93033d1cf66edd1e9a300e7a
Alina Mihut <alinamihut@gmail.com>
2022-05-15 17:14:19 +0300

    OpenCVApplication-VS2019_OCV451_basic

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -5,480 +5,86 @@
 #include "common.h"
 
 
-void testOpenImage()
+void maskRedEyes(Mat img, Mat masked)
 {
-	char fname[MAX_PATH];
-	while(openFileDlg(fname))
-	{
-		Mat src;
-		src = imread(fname);
-		imshow("image",src);
-		waitKey();
-	}
-}
+	float r, g, b, minimum, maximum, C;
+	for (int i = 0; i < img.rows; i++)
+		for (int j = 0; j < img.cols; j++) {
+			unsigned char B, G, R;
+			Vec3b pixel = img.at< Vec3b>(i, j);
+			float H, S, V;
+			B = pixel[0];
+			G = pixel[1];
+			R = pixel[2];
 
+			r = (float)R / 255;
+			b = (float)B / 255;
+			g = (float)G / 255;
 
-/*
-void testOpenImagesFld()
-{
-	char folderName[MAX_PATH];
-	if (openFolderDlg(folderName)==0)
-		return;
-	char fname[MAX_PATH];
-	FileGetter fg(folderName,"bmp");
-	while(fg.getNextAbsFile(fname))
-	{
-		Mat src;
-		src = imread(fname);
-		imshow(fg.getFoundFileName(),src);
-		if (waitKey()==27) //ESC pressed
-			break;
-	}
-}
+			minimum = min(r, min(g, b));
+			maximum = max(r, max(g, b));
 
-void testImageOpenAndSave()
-{
-	Mat src, dst;
+			C = maximum - minimum;
+			V = maximum;
 
-	src = imread("Images/Lena_24bits.bmp", IMREAD_COLOR);	// Read the image
-
-	if (!src.data)	// Check for invalid input
-	{
-		printf("Could not open or find the image\n");
-		return;
-	}
-
-	// Get the image resolution
-	Size src_size = Size(src.cols, src.rows);
-
-	// Display window
-	const char* WIN_SRC = "Src"; //window for the source image
-	namedWindow(WIN_SRC, WINDOW_AUTOSIZE);
-	moveWindow(WIN_SRC, 0, 0);
-
-	const char* WIN_DST = "Dst"; //window for the destination (processed) image
-	namedWindow(WIN_DST, WINDOW_AUTOSIZE);
-	moveWindow(WIN_DST, src_size.width + 10, 0);
-
-	cvtColor(src, dst, COLOR_BGR2GRAY); //converts the source image to a grayscale one
-
-	imwrite("Images/Lena_24bits_gray.bmp", dst); //writes the destination to file
-
-	imshow(WIN_SRC, src);
-	imshow(WIN_DST, dst);
-
-	printf("Press any key to continue ...\n");
-	waitKey(0);
-}
-
-void testNegativeImage()
-{
-	char fname[MAX_PATH];
-	while(openFileDlg(fname))
-	{
-		double t = (double)getTickCount(); // Get the current time [s]
-		
-		Mat src = imread(fname,IMREAD_GRAYSCALE);
-		int height = src.rows;
-		int width = src.cols;
-		Mat dst = Mat(height,width,CV_8UC1);
-		// Asa se acceseaaza pixelii individuali pt. o imagine cu 8 biti/pixel
-		// Varianta ineficienta (lenta)
-		for (int i=0; i<height; i++)
-		{
-			for (int j=0; j<width; j++)
-			{
-				uchar val = src.at<uchar>(i,j);
-				uchar neg = 255 - val;
-				dst.at<uchar>(i,j) = neg;
-			}
-		}
-
-		// Get the current time again and compute the time difference [s]
-		t = ((double)getTickCount() - t) / getTickFrequency();
-		// Print (in the console window) the processing time in [ms] 
-		printf("Time = %.3f [ms]\n", t * 1000);
-
-		imshow("input image",src);
-		imshow("negative image",dst);
-		waitKey();
-	}
-}
-
-void testParcurgereSimplaDiblookStyle()
-{
-	char fname[MAX_PATH];
-	while (openFileDlg(fname))
-	{
-		Mat src = imread(fname, IMREAD_GRAYSCALE);
-		int height = src.rows;
-		int width = src.cols;
-		Mat dst = src.clone();
-
-		double t = (double)getTickCount(); // Get the current time [s]
-
-		// the fastest approach using the “diblook style”
-		uchar *lpSrc = src.data;
-		uchar *lpDst = dst.data;
-		int w = (int) src.step; // no dword alignment is done !!!
-		for (int i = 0; i<height; i++)
-			for (int j = 0; j < width; j++) {
-				uchar val = lpSrc[i*w + j];
-				lpDst[i*w + j] = 255 - val;
-			}
-
-		// Get the current time again and compute the time difference [s]
-		t = ((double)getTickCount() - t) / getTickFrequency();
-		// Print (in the console window) the processing time in [ms] 
-		printf("Time = %.3f [ms]\n", t * 1000);
-
-		imshow("input image",src);
-		imshow("negative image",dst);
-		waitKey();
-	}
-}
-
-void testColor2Gray()
-{
-	char fname[MAX_PATH];
-	while(openFileDlg(fname))
-	{
-		Mat src = imread(fname);
-
-		int height = src.rows;
-		int width = src.cols;
-
-		Mat dst = Mat(height,width,CV_8UC1);
-
-		// Asa se acceseaaza pixelii individuali pt. o imagine RGB 24 biti/pixel
-		// Varianta ineficienta (lenta)
-		for (int i=0; i<height; i++)
-		{
-			for (int j=0; j<width; j++)
-			{
-				Vec3b v3 = src.at<Vec3b>(i,j);
-				uchar b = v3[0];
-				uchar g = v3[1];
-				uchar r = v3[2];
-				dst.at<uchar>(i,j) = (r+g+b)/3;
-			}
-		}
-		
-		imshow("input image",src);
-		imshow("gray image",dst);
-		waitKey();
-	}
-}
-
-void testBGR2HSV()
-{
-	char fname[MAX_PATH];
-	while (openFileDlg(fname))
-	{
-		Mat src = imread(fname);
-		int height = src.rows;
-		int width = src.cols;
-
-		// Componentele d eculoare ale modelului HSV
-		Mat H = Mat(height, width, CV_8UC1);
-		Mat S = Mat(height, width, CV_8UC1);
-		Mat V = Mat(height, width, CV_8UC1);
-
-		// definire pointeri la matricele (8 biti/pixeli) folosite la afisarea componentelor individuale H,S,V
-		uchar* lpH = H.data;
-		uchar* lpS = S.data;
-		uchar* lpV = V.data;
-
-		Mat hsvImg;
-		cvtColor(src, hsvImg, COLOR_BGR2HSV);
-
-		// definire pointer la matricea (24 biti/pixeli) a imaginii HSV
-		uchar* hsvDataPtr = hsvImg.data;
-
-		for (int i = 0; i<height; i++)
-		{
-			for (int j = 0; j<width; j++)
-			{
-				int hi = i*width * 3 + j * 3;
-				int gi = i*width + j;
-
-				lpH[gi] = hsvDataPtr[hi] * 510 / 360;		// lpH = 0 .. 255
-				lpS[gi] = hsvDataPtr[hi + 1];			// lpS = 0 .. 255
-				lpV[gi] = hsvDataPtr[hi + 2];			// lpV = 0 .. 255
-			}
-		}
-
-		imshow("input image", src);
-		imshow("H", H);
-		imshow("S", S);
-		imshow("V", V);
-
-		waitKey();
-	}
-}
-
-
-void testCanny()
-{
-	char fname[MAX_PATH];
-	while(openFileDlg(fname))
-	{
-		Mat src,dst,gauss;
-		src = imread(fname,IMREAD_GRAYSCALE);
-		double k = 0.4;
-		int pH = 50;
-		int pL = (int) k*pH;
-		GaussianBlur(src, gauss, Size(5, 5), 0.8, 0.8);
-		Canny(gauss,dst,pL,pH,3);
-		imshow("input image",src);
-		imshow("canny",dst);
-		waitKey();
-	}
-}
-
-void testVideoSequence()
-{
-	VideoCapture cap("Videos/rubic.avi"); // off-line video from file
-	//VideoCapture cap(0);	// live video from web cam
-	if (!cap.isOpened()) {
-		printf("Cannot open video capture device.\n");
-		waitKey(0);
-		return;
-	}
-		
-	Mat edges;
-	Mat frame;
-	char c;
-
-	while (cap.read(frame))
-	{
-		Mat grayFrame;
-		cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
-		Canny(grayFrame,edges,40,100,3);
-		imshow("source", frame);
-		imshow("gray", grayFrame);
-		imshow("edges", edges);
-		c = waitKey(0);  // waits a key press to advance to the next frame
-		if (c == 27) {
-			// press ESC to exit
-			printf("ESC pressed - capture finished\n"); 
-			break;  //ESC pressed
-		};
-	}
-}
-
-
-void testSnap()
-{
-	VideoCapture cap(0); // open the deafult camera (i.e. the built in web cam)
-	if (!cap.isOpened()) // openenig the video device failed
-	{
-		printf("Cannot open video capture device.\n");
-		return;
-	}
-
-	Mat frame;
-	char numberStr[256];
-	char fileName[256];
-	
-	// video resolution
-	Size capS = Size((int)cap.get(CAP_PROP_FRAME_WIDTH),
-		(int)cap.get(CAP_PROP_FRAME_HEIGHT));
-
-	// Display window
-	const char* WIN_SRC = "Src"; //window for the source frame
-	namedWindow(WIN_SRC, WINDOW_AUTOSIZE);
-	moveWindow(WIN_SRC, 0, 0);
-
-	const char* WIN_DST = "Snapped"; //window for showing the snapped frame
-	namedWindow(WIN_DST, WINDOW_AUTOSIZE);
-	moveWindow(WIN_DST, capS.width + 10, 0);
-
-	char c;
-	int frameNum = -1;
-	int frameCount = 0;
-
-	for (;;)
-	{
-		cap >> frame; // get a new frame from camera
-		if (frame.empty())
-		{
-			printf("End of the video file\n");
-			break;
-		}
-
-		++frameNum;
-		
-		imshow(WIN_SRC, frame);
-
-		c = waitKey(10);  // waits a key press to advance to the next frame
-		if (c == 27) {
-			// press ESC to exit
-			printf("ESC pressed - capture finished");
-			break;  //ESC pressed
-		}
-		if (c == 115){ //'s' pressed - snapp the image to a file
-			frameCount++;
-			fileName[0] = NULL;
-			sprintf(numberStr, "%d", frameCount);
-			strcat(fileName, "Images/A");
-			strcat(fileName, numberStr);
-			strcat(fileName, ".bmp");
-			bool bSuccess = imwrite(fileName, frame);
-			if (!bSuccess) 
-			{
-				printf("Error writing the snapped image\n");
-			}
+			if (V != 0)
+				S = C / V;
 			else
-				imshow(WIN_DST, frame);
-		}
-	}
+				S = 0;
 
-}
-
-void MyCallBackFunc(int event, int x, int y, int flags, void* param)
-{
-	//More examples: http://opencvexamples.blogspot.com/2014/01/detect-mouse-clicks-and-moves-on-image.html
-	Mat* src = (Mat*)param;
-	if (event == EVENT_LBUTTONDOWN)
-		{
-			printf("Pos(x,y): %d,%d  Color(RGB): %d,%d,%d\n",
-				x, y,
-				(int)(*src).at<Vec3b>(y, x)[2],
-				(int)(*src).at<Vec3b>(y, x)[1],
-				(int)(*src).at<Vec3b>(y, x)[0]);
-		}
-}
-
-void testMouseClick()
-{
-	Mat src;
-	// Read image from file 
-	char fname[MAX_PATH];
-	while (openFileDlg(fname))
-	{
-		src = imread(fname);
-		//Create a window
-		namedWindow("My Window", 1);
-
-		//set the callback function for any mouse event
-		setMouseCallback("My Window", MyCallBackFunc, &src);
-
-		//show the image
-		imshow("My Window", src);
-
-		// Wait until user press some key
-		waitKey(0);
-	}
-}
-
-/* Histogram display function - display a histogram using bars (simlilar to L3 / PI)
-Input:
-name - destination (output) window name
-hist - pointer to the vector containing the histogram values
-hist_cols - no. of bins (elements) in the histogram = histogram image width
-hist_height - height of the histogram image
-Call example:
-showHistogram ("MyHist", hist_dir, 255, 200);
-*/
-/*
-void showHistogram(const std::string& name, int* hist, const int  hist_cols, const int hist_height)
-{
-	Mat imgHist(hist_height, hist_cols, CV_8UC3, CV_RGB(255, 255, 255)); // constructs a white image
-
-	//computes histogram maximum
-	int max_hist = 0;
-	for (int i = 0; i<hist_cols; i++)
-	if (hist[i] > max_hist)
-		max_hist = hist[i];
-	double scale = 1.0;
-	scale = (double)hist_height / max_hist;
-	int baseline = hist_height - 1;
-
-	for (int x = 0; x < hist_cols; x++) {
-		Point p1 = Point(x, baseline);
-		Point p2 = Point(x, baseline - cvRound(hist[x] * scale));
-		line(imgHist, p1, p2, CV_RGB(255, 0, 255)); // histogram bins colored in magenta
-	}
-
-	imshow(name, imgHist);
-}
+			if (C != 0) {
+				if (maximum == r) {
+					H = 60 * (g - b) / C;
+				}
+				if (maximum == g) {
+					H = 120 + 60 * (b - r) / C;
+				}
+				if (maximum == b) {
+					H = 240 + 60 * (r - g) / C;
+				}
+			}
+			else {
+				H = 0;
+			}
+			if (H < 0) {
+				H = H + 360;
+			}
 
 
-*/
-
-void maskRedEyes(Mat src, Mat redMask)
-{
-	for (int i = 0; i < src.rows; i++)
-	{
-		for (int j = 0; j < src.cols; j++)
-		{
-			Vec3b pixel = src.at<Vec3b>(i, j); //current pixel of the original image
-
-			float h; //the angle between the current color and the ray corresponding to the color Red
-
-			float r = (float)pixel[2] / 255; //normalized R component
-			float g = (float)pixel[1] / 255; //normalized G component
-			float b = (float)pixel[0] / 255; //normalized B component
-
-			float maximum = max(r, max(g, b));
-			float minimum = min(r, min(g, b));
-			float C = maximum - minimum;
-
-			//calculate hue value
-			if (C != 0)
+			if (((H >= 320 && H <= 360) || (H >= 0 && H <= 5)) && R > (B + G))
 			{
-				if (maximum == r) h = 60 * (g - b) / C;
-				if (maximum == g) h = 120 + 60 * (b - r) / C;
-				if (maximum == b) h = 240 + 60 * (r - g) / C;
+				masked.at<uchar>(i, j) = 255; 
+			}
+			else masked.at<uchar>(i, j) = 0; 
+		}
+}
+
+Mat fixRedEyes(Mat masked, Mat img)
+{
+	Mat dst = img.clone();
+
+	for (int i = 0; i < masked.rows; i++)
+	{
+		for (int j = 0; j < masked.cols; j++)
+		{
+			Vec3b pixel = img.at<Vec3b>(i, j);
+
+			if (masked.at<uchar>(i, j) == 255)
+			{
+				float blue = pixel[0];
+				float green = pixel[1];
+				float mean = (blue + green) / 2;
+
+				dst.at<Vec3b>(i, j) = Vec3b(mean, mean, mean);
 			}
 			else
 			{
-				h = 0;
-			}
-
-			//saturate the hue
-			if (h < 0)
-			{
-				h += 360;
-			}
-
-			//check if the hue is in the range of the red color
-			if ((h >= 320 && h <= 360) || (h >= 0 && h <= 5))
-			{
-				redMask.at<uchar>(i, j) = 255; //color the pixel with white
-			}
-			else redMask.at<uchar>(i, j) = 0; //color the pixel with black
-		}
-	}
-}
-Mat fixRedEyes(Mat redMask, Mat src)
-{
-	Mat fixedImage = src.clone();
-
-	//for each masked pixel average the red and blue channels of the original image and give the resulting value to the fixed image pixel
-	for (int i = 0; i < redMask.rows; i++)
-	{
-		for (int j = 0; j < redMask.cols; j++)
-		{
-			Vec3b pixel = src.at<Vec3b>(i, j);
-
-			if (redMask.at<uchar>(i, j) == 255)
-			{
-				int blue = pixel[0];
-				int green = pixel[1];
-				int average = (blue + green) / 2;
-
-				fixedImage.at<Vec3b>(i, j) = Vec3b(average, average, average);
-			}
-			else
-			{
-				fixedImage.at<Vec3b>(i, j) = pixel;
+				dst.at<Vec3b>(i, j) = pixel;
 			}
 		}
 	}
 
-	return fixedImage;
+	return dst;
 }
 int main()
 {
@@ -492,25 +98,27 @@ int main()
 		
 	}
 
-	imshow("image", img);
-	
+	imshow("Initial image", img);
+
 	// Load HAAR cascade
-	cv::CascadeClassifier eyes_cascade("OpenCV\include\opencv2\data\haar-cascade-files-master\haarcascade_eye.xml");
+	cv::CascadeClassifier eyes_cascade;
 
 	// A vector of Rect for storing bounding boxes for eyes.
 	std::vector<Rect> eyes;
-	if (!eyes_cascade.load("C:/Users/Alina Mihut/Desktop/haarcascade_eye.xml"))
-			{ printf("--(!)Error loading\n");
-			return -1; };
-	//eyes_cascade.load("D:\An3\SEM2\IP\Red_Eye_Removal_v2\OpenCVApplication-VS2019_OCV451_basic\haarcascade_eye.xml");
-	// Detect eyes.
+	if (!eyes_cascade.load("C:/Users/Alina Mihut/Desktop/haarcascade_eye.xml")){
+		printf("Error loading\n");
+		return -1;
+		}
+	
 
-		eyes_cascade.detectMultiScale(img,eyes, 1.3, 4, 0 | CASCADE_SCALE_IMAGE, Size(100, 100));
+	// Detect eyes
+	eyes_cascade.detectMultiScale(img,eyes, 1.3, 4, 0 | CASCADE_SCALE_IMAGE, Size(100, 100));
 	
 
 	Mat redMask(img.rows, img.cols, CV_8UC1);
 	maskRedEyes(img, redMask);
-	imshow("masked photo", redMask);
+	imshow("Red Mask Image", redMask);
+
 	for (size_t i = 0; i < eyes.size(); i++)
 	{
 
@@ -519,18 +127,15 @@ int main()
 			//imshow("eye", eye);
 			Mat interm = eye.clone();
 			maskRedEyes(eye, interm);
-			//imshow("image2", interm);
+			Mat fixedEye = fixRedEyes(interm, eye);
+			//imshow("image2", fixedEye);
 			
 	}
 
-	Mat fixedImage = fixRedEyes(redMask, img);
+	Mat dst = fixRedEyes(redMask, img);
 	
+	imshow("Fixed image", dst);
 
-	imshow("dest", fixedImage);
-
-
-
-	imshow("image", img);
 	waitKey();
 	return 0;
 }
